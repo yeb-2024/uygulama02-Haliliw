10131c87d445e38d43ba1e395d06ca228ff38c8e
Halil İbrahim Unver <halilibrahimunver58@gmail.com>
2025-02-14 18:38:50 +0300

    Override eklendi

--- untitled/src/main/java/org/example/Ödevler/GirisSayfasi.java
+++ untitled/src/main/java/org/example/Ödevler/GirisSayfasi.java
@@ -2,12 +2,15 @@ package org.example.Ödevler;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GirisSayfasi {
+public class GirisSayfasi implements ActionListener {
 
     JFrame frame;
     JTextField kullaniciAdi;
-    JTextField Sifre;
+    JTextField sifre;
+    JButton button;
 
     public GirisSayfasi(){
         JPanel panelUst = new JPanel();
@@ -15,7 +18,7 @@ public class GirisSayfasi {
 
         panelUst.setLayout(new FlowLayout());
         labelUst.setText("Kullanıcı Adı");
-        labelUst.setFont(new Font("Arial",Font.PLAIN, 20));
+        labelUst.setFont(new Font(null,Font.PLAIN, 15));
 
         kullaniciAdi = new JTextField();
         kullaniciAdi.setPreferredSize(new Dimension(200,40));
@@ -23,18 +26,22 @@ public class GirisSayfasi {
         panelUst.add(labelUst);
         panelUst.add(kullaniciAdi);
 
+        button = new JButton("Giriş Yap");
+        button.setFont(new Font(null,Font.PLAIN,15));
+        button.addActionListener(this);
+
         JPanel panelAlt = new JPanel();
         JLabel labelAlt = new JLabel();
 
         panelAlt.setLayout(new FlowLayout());
         labelAlt.setText("Şifre");
-        labelAlt.setFont(new Font("Arial",Font.PLAIN, 20));
+        labelAlt.setFont(new Font(null,Font.PLAIN, 15));
 
-        Sifre = new JTextField();
-        Sifre.setPreferredSize(new Dimension(200,40));
+        sifre = new JTextField();
+        sifre.setPreferredSize(new Dimension(200,40));
 
         panelAlt.add(labelAlt);
-        panelAlt.add(Sifre);
+        panelAlt.add(sifre);
 
         frame = new JFrame("Giriş Sayfası");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -44,5 +51,21 @@ public class GirisSayfasi {
         frame.add(panelUst);
         frame.add(panelAlt);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()== button){
+            String kullaniciAdi = this.kullaniciAdi.getText().trim();
+            String sifre = this.sifre.getText().trim();
+
+            if (kullaniciAdi.isEmpty() || sifre.isEmpty()){
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Lütfen Tüm Alanları Doldurunuz!",
+                        "Hata",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 }
